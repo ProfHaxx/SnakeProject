12246b093a4b23bf929ef79dbc47b7a4160621d2
~ProfHaxx~ <blanc.leedom@protonmail.com>
2020-06-22 11:03:55 +0200

    Basic Structure Generation

---
+++ src/main/java/com/michaeli/snake/App.java
@@ -0,0 +1,13 @@
+package com.michaeli.snake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
