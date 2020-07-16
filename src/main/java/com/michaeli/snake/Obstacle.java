0591acc2fc0323a2ba10d77e66641c003633eac2
Yuchan <yuchan.lee@hotmail.com>
2020-07-16 14:24:30 +0200

    obstacle_semi-update

--- src/main/java/com/michaeli/snake/Obstacle.java
+++ src/main/java/com/michaeli/snake/Obstacle.java
@@ -12,7 +12,8 @@ public class Obstacle {
     BufferedImage[] wall = new BufferedImage[] {
             Utility.getImage("wall/wall_3.png"),
             Utility.getImage("wall/wall_1.png"),
-            Utility.getImage("wall/wall_4.png")
+            Utility.getImage("wall/wall_4.png"),
+            Utility.getImage("wall/wall_2.png")
     };
 
     int id, x, y;
@@ -33,16 +34,29 @@ public class Obstacle {
     public static ArrayList<Obstacle> obstacles = new ArrayList<>();
 
     public static void spawnObstacles() {
-        for(int i = 0; i < (App.WIDTH+App.HEIGHT)/25; i ++) {
+        for(int i = 0; i < (App.WIDTH+App.HEIGHT)/10; i ++) {
             obstacles.add(generate(App.WIDTH / App.COMPONENT_SIZE, App.HEIGHT / App.COMPONENT_SIZE));
         }
     }
 
     public static Obstacle generate (int width, int height) {
         Random random = new Random();
-        int x = random_between(3, width-3);
-        int y = random_between(3, height-3);
-        return new Obstacle(0, x, y);
+        double rand = random.nextDouble();
+        int x = random_between(3, width-4);
+        int y = random_between(3, height-4);
+        if (rand <= 0.2) {
+            return new Obstacle(1, x, y );
+        } else if (rand > 0.2 && rand <= 0.4) {
+            return new Obstacle(0, 3, y);
+        } else if (rand > 0.4 && rand <= 0.6) {
+            return new Obstacle(0, width-4, y);
+        } else if (rand > 0.6 && rand <= 0.8) {
+            return new Obstacle(0, x, 3);
+        } else if (rand > 0.8) {
+            return new Obstacle(0, x, height-4);
+        }
+
+        return new Obstacle (-1,-1,-1);
     }
 
     public static int random_between (int untereGrenze, int obereGrenze) {
@@ -52,7 +66,12 @@ public class Obstacle {
 
     public void paint(Graphics2D g) {
         if(Snake.skin_id > 0) {
-            g.drawImage(wall[Snake.skin_id-1], x*App.COMPONENT_SIZE, y*App.COMPONENT_SIZE, null);
+            if (id == 0) {
+                g.drawImage(wall[Snake.skin_id-1], x*App.COMPONENT_SIZE, y*App.COMPONENT_SIZE, null);
+            }
+            if (id == 1) {
+                g.drawImage(wall[Snake.skin_id-1], x*App.COMPONENT_SIZE, y*App.COMPONENT_SIZE, null);
+            }
         }
     }
 
