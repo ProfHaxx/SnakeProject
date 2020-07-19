408a85f7cf8da37c90f292f5842d45957af6048b
Yuchan <yuchan.lee@hotmail.com>
2020-07-19 17:54:32 +0200

    different maps added

--- src/main/java/com/michaeli/snake/Obstacle.java
+++ src/main/java/com/michaeli/snake/Obstacle.java
@@ -17,6 +17,7 @@ public class Obstacle {
     };
 
     int id, x, y;
+
     public Obstacle (int id, int x, int y) {
         this.id = id; //id lasse ich mal drin, für den Fall, dass wir verschiedene Hindernisse implementieren wollen
         this.x = x;
@@ -36,25 +37,49 @@ public class Obstacle {
     public static void spawnObstacles() {
         obstacles.clear();
         for(int i = 0; i < (App.WIDTH+App.HEIGHT)/10; i ++) {
-            obstacles.add(generate(App.WIDTH / App.COMPONENT_SIZE, App.HEIGHT / App.COMPONENT_SIZE));
+            obstacles.add(generate(1,App.WIDTH / App.COMPONENT_SIZE, App.HEIGHT / App.COMPONENT_SIZE));
         }
     }
 
-    public static Obstacle generate (int width, int height) {
+    public static Obstacle generate (int mapNumber,int width, int height) {
         Random random = new Random();
         double rand = random.nextDouble();
         int x = random_between(3, width-4);
         int y = random_between(3, height-4);
-        if (rand <= 0.2) {
+        if (mapNumber == 1) {
+            if (rand <= 0.4) {
+                return new Obstacle(1, x, y );
+            } else if (rand > 0.4 && rand <= 0.5) {
+                return new Obstacle(0, 2, y);
+            } else if (rand > 0.5 && rand <= 0.6) {
+                return new Obstacle(0, width-4, y);
+            } else if (rand > 0.6 && rand <= 0.8) {
+                return new Obstacle(0, x, 2);
+            } else if (rand > 0.8) {
+                return new Obstacle(0, x, height-4);
+            }
+        } else if (mapNumber == 2) {
+            if (rand <= 0.2) {
+                return new Obstacle(0, 5, y);
+            } else if (rand > 0.2 && rand <= 0.4) {
+                return new Obstacle(0, 10, y);
+            } else if (rand > 0.4 && rand <= 0.6) {
+                return new Obstacle(0, 16, y);
+            } else if (rand > 0.6 && rand <= 0.8) {
+                return new Obstacle(0, 22, y);
+            } else if (rand > 0.8) {
+                return new Obstacle(0, 27, y);
+            }
+        }  else if (mapNumber == 3) {
+            if (rand <= 0.33) {
+                return new Obstacle(0, x, 5);
+            } else if (rand > 0.33 && rand <= 0.66) {
+                return new Obstacle(0, x, 10);
+            } else if (rand > 0.66) {
+                return new Obstacle(0, x, 15);
+            }
+        } else if (mapNumber == 4) {
             return new Obstacle(1, x, y );
-        } else if (rand > 0.2 && rand <= 0.4) {
-            return new Obstacle(0, 3, y);
-        } else if (rand > 0.4 && rand <= 0.6) {
-            return new Obstacle(0, width-4, y);
-        } else if (rand > 0.6 && rand <= 0.8) {
-            return new Obstacle(0, x, 3);
-        } else if (rand > 0.8) {
-            return new Obstacle(0, x, height-4);
         }
 
         return new Obstacle (-1,-1,-1);
